3e32cb0f125241966c0b8357fd4334936ae53f0c
Stephanie Couto <90713207+barracouto@users.noreply.github.com>
2024-07-01 15:46:06 -0400

    Update UntaggedLargeInstances.py

--- LambdaFunctions/UntaggedLargeInstances.py
+++ LambdaFunctions/UntaggedLargeInstances.py
@@ -3,7 +3,6 @@ import boto3
 def lambda_handler(event, context):
     ec2 = boto3.client('ec2')
     
-    # Describe all running instances
     response = ec2.describe_instances(
         Filters=[
             {'Name': 'instance-state-name', 'Values': ['running']}
